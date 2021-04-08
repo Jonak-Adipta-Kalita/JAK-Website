7a92ba65f9139761d0d68872844760f6a1b25387
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-04-08 09:57:55 +0530

    Added Google ReCaptcha

--- main_website/settings.py
+++ main_website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '(8s-_e&&b#&&8!!asuuafog(o%*l)3cufrpnzvr$u@h6fj@8k_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jonakadiptakalita.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles'
-    
 ]
 
 MIDDLEWARE = [
