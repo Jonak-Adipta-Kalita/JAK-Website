61531fc508c4c5d1c4240fb6ba69dd79836be896
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-04-13 14:54:55 +0530

    Update!!

--- main_website/settings.py
+++ main_website/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MESSAGE_TAGS = {
    messages.ERROR: 'danger'
 }
