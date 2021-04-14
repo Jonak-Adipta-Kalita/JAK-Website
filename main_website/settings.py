3e30f7dcd454ad9e8eeea780af00c9e15e48db94
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-04-14 12:32:11 +0530

    update

--- main_website/settings.py
+++ main_website/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '(8s-_e&&b#&&8!!asuuafog(o%*l)3cufrpnzvr$u@h6fj@8k_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['jonakadiptakalita.herokuapp.com', 'jonakadiptakalita.com', 'localhost', '127.0.0.1']
 
