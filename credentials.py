91484be390d09ed670b4b9c442276bd307d9d26a
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-04-27 22:16:27 +0530

    added credential.py file

--- credentials.py
+++ credentials.py
@@ -1,3 +1,3 @@
 PORTS = ['jonakadiptakalita.herokuapp.com', 'jonakadiptakalita.com', 'localhost', '127.0.0.1']
-SECRET_KEY = '(8s-_e&&b#&&8!!asuuafog(o%*l)3cufrpnzvr$u@h6fj@8k_'
-RECAPTCHA_SECRET_KEY = '6LdC8KAaAAAAALYaDETpXQxAaX50_LM7Dlw29n6o'
+SECRET_KEY = "(8s-_e&&b#&&8!!asuuafog(o%*l)3cufrpnzvr$u@h6fj@8k_"
+RECAPTCHA_SECRET_KEY = "6LdC8KAaAAAAALYaDETpXQxAaX50_LM7Dlw29n6o"
