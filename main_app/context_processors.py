8f4e5eaab6851a2751ac96ca0254da4019fa5c53
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-05-01 07:50:56 +0530

    Major Update

--- main_app/context_processors.py
+++
@@ -1,7 +0,0 @@
-from .models import Notification
-
-def add_notificationsUnread_to_context(request):
-    notifications_unread = Notification.objects.all().count()
-    return {
-        'notifications_unread': notifications_unread
-    }
--- main_website/settings.py
+++ main_website/settings.py
@@ -66,7 +66,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'main_app.context_processors.add_notificationsUnread_to_context',
             ],
         },
     },
