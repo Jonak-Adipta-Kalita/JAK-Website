b3c8f1ff5204d0ca73866be9519b26a563d4208a
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-05-03 14:34:34 +0530

    added all the css and the js in other separate files

---
+++ static/scripts/home.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+    var typed = new Typed('#typed', {
+        strings: [
+			"I'm a Coder",
+			"I'm a Student",
+			"I'm an Assamese",
+			"I'm a Designer",
+			"I'm a Gamer",
+			"I'm a Sushi Lover",
+		],
+        backSpeed: 30,
+        smartBackspace: true,
+        backDelay: 1500,
+        startDelay: 1000,
+        typeSpeed: 25,
+        loop: true,
+    });
+};
