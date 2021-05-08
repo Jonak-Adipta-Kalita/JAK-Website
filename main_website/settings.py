b7887b691775e90ab4cab30504c74b7c2aaf4f69
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-05-08 12:30:48 +0530

    added context_processors.py

--- main_website/settings.py
+++ main_website/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'main_app.context_processors.RECAPTCHA_CLIENT_KEY_Func',
             ],
         },
     },
