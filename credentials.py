1d10f10b46b52c47da5954470a7cd09afbe61a0b
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-05-19 14:58:26 +0530

    deleted credentials.py

--- credentials.py
+++
@@ -1,4 +0,0 @@
-PORTS = ['jonakadiptakalita.herokuapp.com', 'jonakadiptakalita.com', 'localhost', '127.0.0.1']
-SECRET_KEY = "(8s-_e&&b#&&8!!asuuafog(o%*l)3cufrpnzvr$u@h6fj@8k_"
-RECAPTCHA_SECRET_KEY = "6LdC8KAaAAAAALYaDETpXQxAaX50_LM7Dlw29n6o"
-RECAPTCHA_CLIENT_KEY = "6LdC8KAaAAAAALZ2keQ9F4S5gnXwC9xXRG3rxQjT"
