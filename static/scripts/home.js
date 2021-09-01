7839df3d4a90ecdd4f69bc4a5d54a1fd60e6a9f1
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-09-01 08:35:28 +0530

    lets use my style for coding

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,13 +1,15 @@
-window.onload = function () {
-    var typed = new Typed('#typed', {
-        strings: [
-			"I'm a Coder",
-			"I'm a Student",
-			"I'm an Assamese",
-			"I'm a Designer",
-			"I'm a Gamer",
-			"I'm a Sushi Lover",
-		],
+const stringsArray = [
+	"I'm a Coder",
+	"I'm a Student",
+	"I'm an Assamese",
+	"I'm a Designer",
+	"I'm a Gamer",
+	"I'm a Sushi Lover",
+];
+
+window.onload = () => {
+    let typed = new Typed("#typed", {
+        strings: stringsArray,
         backSpeed: 30,
         smartBackspace: true,
         backDelay: 1500,
