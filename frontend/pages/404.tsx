a10d4143983f6bdb8d3ec781128fc8961c40be3d
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-01-31 21:31:20 +0530

    added Typed.js

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -4,15 +4,15 @@ import Header from "../components/Header";
 
 const _404 = () => {
     return (
-        <div className="flex flex-col h-screen bg-[#272934]">
+        <div className="flex h-screen flex-col bg-[#272934]">
             <Head>
                 <title>JAK Discord Bot | Page Not Found</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Header />
-            <main className="flex-1 overflow-y-auto scrollbar-hide bg-[#272934] px-2 md:px-4 lg:px-6 xl:px-10">
+            <main className="flex-1 overflow-y-auto bg-[#272934] px-2 scrollbar-hide md:px-4 lg:px-6 xl:px-10">
                 <div className="flex justify-center">
-                    <h1 className="text-white font-bold text-2xl">
+                    <h1 className="text-2xl font-bold text-white">
                         Page Not Found!!
                     </h1>
                 </div>
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Typed from "react-typed";
 
 const Home = () => {
     return (
@@ -10,7 +11,30 @@ const Home = () => {
             </Head>
             <Header />
             <main className="flex-1 overflow-y-auto scrollbar-hide">
-                <div className=""></div>
+                <div className="mt-[50px]">
+                    <div className="flex justify-center">
+                        <h3 className="text-3xl">
+                            <Typed
+                                strings={[
+                                    "I'm a Programmer",
+                                    "I'm a Student",
+                                    "I'm an Assamese",
+                                    "I'm a Designer",
+                                    "I'm a Gamer",
+                                    "I'm a Sushi Lover",
+                                ]}
+                                backSpeed={30}
+                                smartBackspace={true}
+                                backDelay={1500}
+                                startDelay={1000}
+                                typeSpeed={25}
+                                loop={true}
+                                className="mb-[50px] font-bold"
+                            />
+                        </h3>
+                    </div>
+                    <div className=""></div>
+                </div>
             </main>
             <Footer />
         </div>
