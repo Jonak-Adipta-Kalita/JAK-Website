267f6613ebac65bdd2ef910897f11e178259628b
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-01-31 17:38:20 +0530

    sticky header and footer!!

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+    return <header className=""></header>;
+};
+
+export default Header;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,19 @@
 import Head from "next/head";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 const Home = () => {
     return (
-        <div className="">
+        <div className="flex h-screen flex-col">
             <Head>
                 <title>JAK Website | Home</title>
                 <link rel="icon" href="/images/favicon.png" />
             </Head>
+            <Header />
+            <main className="flex-1 overflow-y-auto scrollbar-hide">
+                <div className=""></div>
+            </main>
+            <Footer />
         </div>
     );
 };
