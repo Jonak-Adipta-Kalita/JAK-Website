c2e166832d1a5b75572a402cdbfc9822d53e72aa
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-02-03 09:33:45 +0530

    :(

--- backend/main_app/views.py
+++ backend/main_app/views.py
@@ -77,8 +77,6 @@ class MyPhotoDetailAPIView(generics.GenericAPIView):
 
 class ContactAPIView(generics.CreateAPIView):
     serializer_class = ContactSerializer
-    authentication_classes = [authentication.SessionAuthentication, authentication.BasicAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return Contact.objects.all()
