1282fd235df3185f6123af310172b6eeb7f8cae7
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-02-06 19:29:48 +0530

    lets go with google auth :D

--- backend/main_app/urls.py
+++ backend/main_app/urls.py
@@ -8,15 +8,15 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("api/games/own", views.GameOwnListAPIView.as_view(), name="own_games"),
     path(
-        "api/games/own/<str:id>", views.GameOwnDetailAPIView.as_view(), name="own_games"
+        "api/games/own/<str:id>", views.GameOwnDetailAPIView.as_view(), name="own_games_search"
     ),
     path("api/games/fav", views.GameFavListAPIView.as_view(), name="fav_games"),
     path(
-        "api/games/fav/<str:id>", views.GameFavDetailAPIView.as_view(), name="fav_games"
+        "api/games/fav/<str:id>", views.GameFavDetailAPIView.as_view(), name="fav_games_searcg"
     ),
     path("api/my_photos", views.MyPhotoListAPIView.as_view(), name="my_photos"),
     path(
-        "api/my_photos/<str:id>", views.MyPhotoDetailAPIView.as_view(), name="my_photos"
+        "api/my_photos/<str:id>", views.MyPhotoDetailAPIView.as_view(), name="my_photos_search"
     ),
     path(
         "api/notifications",
