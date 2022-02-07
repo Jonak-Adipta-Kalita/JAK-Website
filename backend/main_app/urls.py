fb363c36f229b821947d633f60f8088e86f24b25
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-02-07 07:18:27 +0530

    lets hope that i can do the authentication

--- backend/main_app/urls.py
+++ backend/main_app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from rest_framework.authtoken import views as authtoken_views
 from django.conf import settings
 from django.conf.urls.static import static
 
