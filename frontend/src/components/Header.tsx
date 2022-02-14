678f61f24c8b623ebfc768e326c9a9e236b2c71a
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-02-14 12:48:44 +0530

    nice!!

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,8 @@
-import { useState, FormEvent } from "react";
+import { useState, FormEvent, Fragment } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { SearchIcon } from "@heroicons/react/solid";
+import { SearchIcon, ChevronDownIcon } from "@heroicons/react/solid";
+import { Menu, Transition } from "@headlessui/react";
 import {
     loginModalState,
     signUpModalState,
@@ -17,6 +18,7 @@ const Header = () => {
     const [loggedOut, setLoggedOut] = useState(false);
     const setLoginModalOpen = useSetRecoilState(loginModalState);
     const setSignUpModalOpen = useSetRecoilState(signUpModalState);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
 
     const search = (e: MouseEvent | FormEvent) => {
         e.preventDefault();
@@ -28,7 +30,7 @@ const Header = () => {
         setSearchQuery("");
     };
 
-    const logout = async (e: MouseEvent) => {
+    const logout = async (e: any) => {
         e.preventDefault();
 
         setSession({
@@ -120,7 +122,66 @@ const Header = () => {
                         </button>
                     </div>
                 ) : (
-                    <div className=""></div>
+                    <div
+                        className="flex cursor-pointer items-center space-x-3"
+                        onClick={() => setDropdownOpen(!dropdownOpen)}
+                    >
+                        <Menu
+                            as="div"
+                            className="relative inline-block text-left"
+                        >
+                            <div className="">
+                                <Menu.Button className="flex items-center text-gray-400 hover:text-white">
+                                    {session.user?.first_name}
+                                    <ChevronDownIcon
+                                        className="-mr-1 ml-2 h-5 w-5"
+                                        aria-hidden="true"
+                                    />
+                                </Menu.Button>
+                            </div>
+
+                            <Transition
+                                as={Fragment}
+                                enter="transition ease-out duration-100"
+                                enterFrom="transform opacity-0 scale-95"
+                                enterTo="transform opacity-100 scale-100"
+                                leave="transition ease-in duration-75"
+                                leaveFrom="transform opacity-100 scale-100"
+                                leaveTo="transform opacity-0 scale-95"
+                            >
+                                <Menu.Items className="absolute right-0 mt-6 w-56 origin-top-right divide-y divide-gray-100 rounded-md border-[0.1px] bg-[#272934] text-white shadow-lg">
+                                    <div className="py-1">
+                                        <Menu.Item>
+                                            {() => (
+                                                <p
+                                                    onClick={() =>
+                                                        router.push(
+                                                            "/dashboard"
+                                                        )
+                                                    }
+                                                    className="menu-item"
+                                                >
+                                                    Dashboard
+                                                </p>
+                                            )}
+                                        </Menu.Item>
+                                    </div>
+                                    <div className="py-1">
+                                        <Menu.Item>
+                                            {() => (
+                                                <p
+                                                    onClick={(e) => logout(e)}
+                                                    className="block cursor-pointer px-4 py-2 text-sm font-semibold text-red-700 hover:text-red-500"
+                                                >
+                                                    Logout
+                                                </p>
+                                            )}
+                                        </Menu.Item>
+                                    </div>
+                                </Menu.Items>
+                            </Transition>
+                        </Menu>
+                    </div>
                 )}
             </div>
         </header>
