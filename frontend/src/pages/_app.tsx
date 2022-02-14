544fb33dea7018783b7e6deb08c19ecb5eb5d51d
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-02-14 16:11:12 +0530

    api endpoints for frontend complete ig!!

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -16,12 +16,12 @@ Router.events.on("routeChangeStart", progress.start);
 Router.events.on("routeChangeComplete", progress.finish);
 Router.events.on("routeChangeError", progress.finish);
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
     return (
         <RecoilRoot>
             <Component {...pageProps} />
         </RecoilRoot>
     );
-}
+};
 
 export default MyApp;
---
+++ frontend/src/pages/api/auth/verify.ts
@@ -0,0 +1,50 @@
+import cookie from "cookie";
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if (!(req.method === "GET")) {
+        res.setHeader("Allow", ["GET"]);
+        return res
+            .status(405)
+            .json({ error: `Method ${req.method} now allowed` });
+    }
+
+    const cookies = cookie.parse(req.headers.cookie ?? "");
+    const access = cookies.access ?? false;
+
+    if (!access) {
+        return res.status(403).json({
+            error: "User forbidden from making the request",
+        });
+    }
+
+    try {
+        const apiRes = await axios.post(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/token/verify`,
+            JSON.stringify({
+                token: access,
+            }),
+            {
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+
+        if (apiRes.status === 200) {
+            return res
+                .status(200)
+                .json({ success: "Authenticated successfully!!" });
+        } else {
+            return res.status(apiRes.status).json({
+                error: "Failed to authenticate!!",
+            });
+        }
+    } catch (error) {
+        return res.status(500).json({
+            error: "Something went wrong when trying to authenticate!!",
+        });
+    }
+};
