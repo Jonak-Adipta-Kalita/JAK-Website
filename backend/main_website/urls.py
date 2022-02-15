8129ef191b91d6b277acb458a72a16f20fc603f7
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-02-15 11:44:10 +0530

    forgot to add media and static urls

--- backend/main_website/urls.py
+++ backend/main_website/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+from django.conf import settings
+from django.conf.urls import url
 
 admin.site.site_header = "Developer: Jonak"
 admin.site.site_title = ""
@@ -8,4 +11,6 @@ admin.site.index_title = "JAK Website"
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(r"", include("main_app.urls")),
+    url(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    url(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ]
