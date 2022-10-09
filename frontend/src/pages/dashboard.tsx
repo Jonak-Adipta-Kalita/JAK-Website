b746ed2762359b51ef297dabe803c7cc4370b050
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-10-09 22:32:57 +0530

    lets make the change_password endpoint!!

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,9 +1,44 @@
+import HCaptcha from "@hcaptcha/react-hcaptcha";
 import Head from "next/head";
-import { useRecoilValue } from "recoil";
+import { MouseEvent, useRef, useState } from "react";
+import { useRecoilState } from "recoil";
 import { sessionState } from "../atoms/authAtom";
 
 const Dashbaord = () => {
-    const session = useRecoilValue(sessionState);
+    const [session, setSession] = useRecoilState(sessionState);
+    const [currentPassword, setCurrentPassword] = useState<string>("");
+    const [newPassword, setNewPassword] = useState<string>("");
+    const [hCaptchaToken, setHCaptchaToken] = useState("");
+    const captchaRef = useRef<any>(null);
+
+    const changePassword = (e: MouseEvent) => {
+        e.preventDefault();
+
+        setSession({
+            ...session,
+            isLoading: true,
+        });
+
+        if (!hCaptchaToken) {
+            setSession({
+                ...session,
+                isLoading: false,
+            });
+            return;
+        }
+        try {
+            // Change the password
+            setCurrentPassword("");
+            setNewPassword("");
+        } catch (error) {
+            alert("Something went Wrong, when Changing Password!!");
+        }
+
+        setSession({
+            ...session,
+            isLoading: false,
+        });
+    };
 
     return (
         <main className="flex-1 overflow-y-auto scrollbar-hide">
@@ -35,6 +70,56 @@ const Dashbaord = () => {
                             <p className="dashboardText">
                                 {session.user?.email}
                             </p>
+                        </div>
+                        <div className="ml-[50px] pt-[40px]">
+                            <p className="text-2xl underline underline-offset-8">
+                                Change Password
+                            </p>
+                            <div className="flex flex-col space-x-4 md:flex-row">
+                                <div className="mt-[20px] flex flex-col space-y-2">
+                                    <input
+                                        type="password"
+                                        required
+                                        value={currentPassword}
+                                        onChange={(e) =>
+                                            setCurrentPassword(e.target.value)
+                                        }
+                                        className="authInput"
+                                        placeholder="Current Password"
+                                    />
+                                    <input
+                                        type="new-password"
+                                        required
+                                        value={newPassword}
+                                        onChange={(e) =>
+                                            setNewPassword(e.target.value)
+                                        }
+                                        className="authInput"
+                                        placeholder="New Password"
+                                    />
+                                    <HCaptcha
+                                        sitekey={
+                                            process.env
+                                                .NEXT_PUBLIC_HCAPTCHA_SITE_KEY!
+                                        }
+                                        onVerify={setHCaptchaToken}
+                                        onLoad={() => {
+                                            captchaRef.current.execute();
+                                        }}
+                                        ref={captchaRef}
+                                        theme="dark"
+                                    />
+                                </div>
+                                <div className="relative">
+                                    <button
+                                        onClick={(e) => changePassword(e)}
+                                        className="absolute left-20 mt-5 rounded-lg border-[2px] border-gray-400 p-5 md:left-0 md:bottom-20 md:mt-0"
+                                        aria-label="change-password"
+                                    >
+                                        Change
+                                    </button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
