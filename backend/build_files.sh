8940dc2a86c97afeb5c990e5591b50382a11fb28
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-11-25 16:45:53 +0530

    there are no static files currently!!

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,2 +1,2 @@
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+#python3.9 manage.py collectstatic
--- backend/main_website/settings.py
+++ backend/main_website/settings.py
@@ -29,8 +29,7 @@ MEDIA_URL = "/media/"
 STATIC_URL = "/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 ROOT_URLCONF = "main_website.urls"
 
