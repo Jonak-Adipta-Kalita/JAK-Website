6be8cb730524ff2b441905c13e792a5a8bfe2421
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-12-19 18:22:15 +0530

    gotta add the search bar in...

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -208,7 +208,7 @@ const Header = () => {
                             </div>
                         ) : (
                             <div
-                                className="flex cursor-pointer items-center space-x-3"
+                                className="z-50 flex cursor-pointer items-center space-x-3"
                                 onClick={() => setDropdownOpen(!dropdownOpen)}
                             >
                                 <Menu
@@ -234,7 +234,7 @@ const Header = () => {
                                         leaveFrom="transform opacity-100 scale-100"
                                         leaveTo="transform opacity-0 scale-95"
                                     >
-                                        <Menu.Items className="absolute right-0 mt-6 w-56 origin-top-right divide-y divide-gray-100 rounded-md border-[0.1px] bg-bg-color-light text-white shadow-lg dark:bg-bg-color-dark">
+                                        <Menu.Items className="absolute right-0 mt-6 w-56 origin-top-right divide-y divide-gray-100 rounded-md border-[0.1px] bg-bg-color-light text-white shadow-lg dark:bg-bg-color-dark z-50">
                                             <div className="py-1">
                                                 <Menu.Item>
                                                     {() => (
