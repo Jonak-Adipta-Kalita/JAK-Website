d6b61c6bcd8c09b15d6f2a475fe92140d14b18f1
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-01-03 21:17:17 +0530

    added a z index of 50 to the theme toggle button

--- frontend/src/components/ToggleThemeButton.tsx
+++ frontend/src/components/ToggleThemeButton.tsx
@@ -14,7 +14,7 @@ const ToggleThemeButton = () => {
     return (
         <button
             type="button"
-            className="absolute top-28 right-[16px] w-fit rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+            className="absolute top-28 right-[16px] w-fit rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700 z-50"
             onClick={() => setTheme(theme === "light" ? "dark" : "light")}
 			aria-label="theme-toggle"
         >
