cb228543329a420419b2fb6edc3fd7526d207338
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-06-12 22:10:21 +0530

    nah man, I will follow my own path.....

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,5 @@
-import Header from "@components/Header";
-import Hero from "@components/Hero";
-import { api } from "@xxjonakadiptaxx/jak_javascript_package";
-
 const Home = async () => {
-    const socials = (
-        await new api(process.env.RAPIDAPI_KEY!).getJAK()
-    ).social_medias.filter((social) => social.value !== "Discord");
-
-    return (
-        <div className="h-screen bg-[rgb(36,36,36)] text-white">
-            <Header socials={socials} />
-            <section id="hero">{/* <Hero /> */}</section>
-        </div>
-    );
+    return <div className=""></div>;
 };
 
 export default Home;
--- frontend/src/utils/getSocials.ts
+++
@@ -1,9 +0,0 @@
-import { api } from "@xxjonakadiptaxx/jak_javascript_package";
-
-export const getSocials = async () => {
-    "use server";
-
-    return (
-        await new api(process.env.RAPIDAPI_KEY!).getJAK()
-    ).social_medias.filter((social) => social.value !== "Discord");
-};
