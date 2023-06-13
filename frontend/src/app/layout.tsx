fe7762f547085dc3b0ae2a9f8580a91f77ee133a
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-06-13 08:37:42 +0530

    gonna start building as a dark website, then implement light!

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body className="h-screen bg-[#282C33]">{children}</body>
         </html>
     );
 }
