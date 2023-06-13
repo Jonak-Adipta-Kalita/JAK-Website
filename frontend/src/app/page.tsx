a77c59b8bbd8d2559de4ed28224b0c2fd2043a72
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-06-13 18:22:28 +0530

    nah, i gotta think something different for the content.....

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,7 @@
-import Header from "@components/Header";
-
 const Home = async () => {
     return (
         <div className="m-auto max-w-6xl rounded-xl p-8 shadow-2xl">
-            <div>
-                <Header />
-            </div>
+            <div></div>
         </div>
     );
 };
