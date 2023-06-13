2ab524c1089bff54e5b25d36f85d1d37bde18bd0
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-06-13 19:31:20 +0530

    that looks soo slick!

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,15 @@
 const Home = async () => {
     return (
-        <div className="m-auto max-w-6xl rounded-xl p-8 shadow-2xl">
-            <div className="space-y-5 text-left">
+        <div className="m-auto flex max-w-6xl flex-col items-center justify-between rounded-xl p-8 shadow-2xl md:flex-row">
+            <div className="space-y-4 self-start text-left">
                 <p className="text-2xl font-bold text-slate-200 sm:text-[35px] md:text-[37px]">
                     Jonak Adipta Kalita
                 </p>
-                <p className="sm:text-md font-semibold md:text-base">
+                <p className="text-xs font-semibold text-slate-200 sm:text-base md:text-lg">
                     A Full Stack Web Developer
+                </p>
+                <p className="text-xs text-slate-300/50 sm:text-base">
+                    I am a 15 y/o Teenager, who loves to make Cool Projects
                 </p>
             </div>
             <div></div>
