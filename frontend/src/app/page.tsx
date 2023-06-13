82ceadc64ea5d1fd4a2e9bd785c512ad37aab7ba
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-06-13 21:12:03 +0530

    gonna put the socials in the footer

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,40 @@
+// import { api } from "@xxjonakadiptaxx/jak_javascript_package";
+
 const Home = async () => {
+    // const socials = (await new api(process.env.RAPIDAPI_KEY!).getJAK())
+    //     .social_medias;
+
     return (
-        <div className="m-auto flex max-w-6xl flex-col items-center justify-between rounded-xl p-8 shadow-2xl md:flex-row">
-            <div className="space-y-3 self-start text-left">
-                <p className="text-2xl font-bold text-slate-200 sm:text-[35px] md:text-[37px]">
-                    Jonak Adipta Kalita
-                </p>
-                <p className="text-xs font-semibold text-slate-200 sm:text-base md:text-lg">
-                    A Full Stack Web Developer
-                </p>
-                <p className="text-xs text-slate-300/50 sm:text-base">
-                    I am a 15 y/o Teenager, who loves to make Cool Projects
-                </p>
+        <div className="m-auto max-w-6xl space-y-10 rounded-xl p-8 shadow-2xl">
+            <div className="flex flex-col items-center justify-between gap-y-5 lg:flex-row">
+                <div className="space-y-3">
+                    <p className="text-2xl font-bold text-slate-200 sm:text-[35px] md:text-[37px]">
+                        Jonak Adipta Kalita
+                    </p>
+                    <p className="text-xs font-semibold text-slate-200 sm:text-base md:text-lg">
+                        A Full Stack Web Developer
+                    </p>
+                    <p className="text-xs text-slate-300/50 sm:text-base">
+                        I am a 15 y/o Teenager, who loves to make Cool Projects
+                    </p>
+                </div>
             </div>
-            <div></div>
+            {/* <div className="flex items-center justify-center space-x-5">
+                {socials.map((social) => (
+                    <a
+                        key={social.id}
+                        href={social.link}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img
+                            alt={social.value}
+                            src={"https://jak-api.vercel.app" + social.imageURL}
+                            className="h-5 md:h-7 lg:h-10"
+                        />
+                    </a>
+                ))}
+            </div> */}
         </div>
     );
 };
