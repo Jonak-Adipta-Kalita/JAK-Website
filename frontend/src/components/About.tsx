d5daf6873b5c62fd297e6e042f9e9436db69af8e
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-06-14 09:43:34 +0530

    lets work on the about me section :)

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -5,11 +5,36 @@ import { motion } from "framer-motion";
 const About = () => {
     return (
         <motion.div
-            className=""
+            className="space-y-5 text-justify text-slate-400"
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
+            transition={{ duration: 1.5 }}
         >
-            <p className="text-justify text-slate-300/50"></p>
+            <p>
+                I am a 10th standard student at Kendriya Vidyalaya Mangaldoi,
+                with a deep passion for programming and gaming. Python and
+                TypeScript (JavaScript) are my favorite programming languages,
+                and I enjoy exploring their possibilities.
+            </p>
+            <p>
+                Gaming is another interest of mine, and I find great joy in
+                playing Need For Speed, Assassin&apos;s Creed, and more. These
+                games provide an exciting escape and also help me develop
+                problem-solving and strategic thinking skills. Additionally, I
+                am an avid anime fan, with favorites like Pokemon, Beyblade, and
+                Dragon Ball Z. The captivating storylines, unique art styles,
+                and memorable characters of anime series fascinate me.
+            </p>
+            <p>
+                Balancing my academic studies with my programming, gaming, and
+                anime interests is important to me. These activities fuel my
+                creativity, broaden my horizons, and provide a much-needed break
+                from routine. I am constantly seeking new avenues within these
+                realms to expand my knowledge, skills, and experiences. Whether
+                it&apos;s coding, gaming, or immersing myself in the world of
+                anime, I find joy and inspiration in every aspect of my diverse
+                interests.
+            </p>
         </motion.div>
     );
 };
