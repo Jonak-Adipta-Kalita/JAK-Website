3842780fde5afab8aabe1f7b2c51b75675622f68
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-06-14 08:37:40 +0530

    sick animation for the header in desktop!

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import About from "@components/About";
 import Navigation from "@components/Navigation";
+import MainHeader from "@components/MainHeader";
 
 const Home = async () => {
     // const socials = (await new api(process.env.RAPIDAPI_KEY!).getJAK())
@@ -12,33 +13,7 @@ const Home = async () => {
             <div className="flex items-center justify-center lg:hidden">
                 <Navigation />
             </div>
-            <div className="flex flex-col items-center justify-between gap-y-5 lg:flex-row">
-                <div className="space-y-3">
-                    <p className="text-2xl font-bold text-slate-200 sm:text-[35px] md:text-[37px]">
-                        Jon
-                        <span className="custom-underline decoration-[3px]">
-                            ak Adipta Ka
-                        </span>
-                        lita
-                    </p>
-                    <p className="text-xs font-semibold text-slate-200 sm:text-base md:text-lg">
-                        A Full Stack Web Developer
-                    </p>
-                    <p className="text-xs text-slate-300/50 sm:text-base">
-                        I am a{" "}
-                        <span className="custom-underline decoration-[0.2px] underline-offset-[4px]">
-                            15 y/o Teenager
-                        </span>
-                        , who loves to make{" "}
-                        <span className="custom-underline decoration-[0.2px] underline-offset-[4px]">
-                            Cool Projects
-                        </span>
-                    </p>
-                </div>
-                <div className="hidden lg:inline">
-                    <Navigation />
-                </div>
-            </div>
+            <MainHeader />
             <section id="about">
                 <About />
             </section>
---
+++ frontend/src/components/MainHeader.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Navigation from "@components/Navigation";
+import { motion } from "framer-motion";
+
+const MainHeader = () => {
+    return (
+        <div className="flex flex-col items-center justify-between gap-y-5 lg:flex-row">
+            <motion.div
+                className="space-y-3"
+                initial={{
+                    x: -500,
+                    opacity: 0,
+                    scale: 0.5,
+                }}
+                animate={{ x: 0, opacity: 1, scale: 1 }}
+                transition={{ duration: 0.5 }}
+            >
+                <p className="text-2xl font-bold text-slate-200 sm:text-[35px] md:text-[37px]">
+                    Jon
+                    <span className="custom-underline decoration-[3px]">
+                        ak Adipta Ka
+                    </span>
+                    lita
+                </p>
+                <p className="text-xs font-semibold text-slate-200 sm:text-base md:text-lg">
+                    A Full Stack Web Developer
+                </p>
+                <p className="text-xs text-slate-300/50 sm:text-base">
+                    I am a{" "}
+                    <span className="custom-underline decoration-[0.2px] underline-offset-[4px]">
+                        15 y/o Teenager
+                    </span>
+                    , who loves to make{" "}
+                    <span className="custom-underline decoration-[0.2px] underline-offset-[4px]">
+                        Cool Projects
+                    </span>
+                </p>
+            </motion.div>
+            <motion.div
+                className="hidden lg:inline"
+                initial={{
+                    x: 500,
+                    opacity: 0,
+                    scale: 0.5,
+                }}
+                animate={{ x: 0, opacity: 1, scale: 1 }}
+                transition={{ duration: 1 }}
+            >
+                <Navigation />
+            </motion.div>
+        </div>
+    );
+};
+
+export default MainHeader;
