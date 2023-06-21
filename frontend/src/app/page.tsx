eb57b1611f1da852b5a4bf17b7829e5af20b5af0
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-06-21 17:30:14 +0530

    UNDER MAINTENANCE!!!

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,15 @@
 const Home = async () => {
-    return <div></div>;
+    return (
+        <div>
+            <div className="flex h-screen flex-col items-center justify-center">
+                <h1 className="text-4xl font-bold">Under Maintenance</h1>
+                <p className="text-xl">
+                    This website is currently under maintenance. Please check
+                    back later.
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
