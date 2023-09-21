c9a59be2df74d254cc067aa5a83e59cfea69760b
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-09-21 10:06:38 +0530

    this shit complicated lol

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,13 @@
 const Navbar = () => {
-    return <div className="flex-[0.224] bg-[#F3F4F6] dark:bg-[#03060F]"></div>;
+    return (
+        <div className="hidden flex-[0.224] overflow-hidden bg-[#F3F4F6] dark:bg-[#03060F] md:inline">
+            <div></div>
+            {/* <hr className="bg-[#E9EBED] p-[10px] dark:bg-[#060913]" /> */}
+            <div></div>
+            {/* <hr className="bg-[#E9EBED] p-[10px] dark:bg-[#060913]" /> */}
+            <div></div>
+        </div>
+    );
 };
 
 export default Navbar;
