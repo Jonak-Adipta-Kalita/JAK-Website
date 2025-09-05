9f5301d995af91c97d3d11c11fff975fdbbd08ce
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2025-09-05 10:47:07 +0530

    gosh my brain hurts trying to figure out a way of doin this stupid responsive thing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ export default function Home() {
         <div>
             <div>
                 <div className="font-salsa text-center">
-                    <p className="md:text-[clamp(3rem,2.92vw+1.6rem,4.4rem)] text-[1.5rem] leading-[1] text-fg-extradark tracking-[clamp(-5.3px,-0.28vw-1px,-1px)]">
+                    <p className="text-[clamp(1.459rem,0.606vw+1.459rem,1.75rem)] sm:text-[clamp(3rem,2.92vw+1.6rem,4.4rem)] leading-[1] text-fg-extradark tracking-[clamp(-5.3px,-0.28vw-1px,-1px)]">
                         Gamer && Programmer && Musician
                     </p>
-                    <p className="text-[clamp(1.25rem,1.302vw+0.625rem,1.875rem)] text-sm text-fg-light tracking-wide xl:mt-8 mt-1 text-center">
+                    <p className="hidden text-[clamp(1.25rem,1.302vw+0.625rem,1.875rem)] text-sm text-fg-light tracking-wide xl:mt-5 mt-1 text-center">
                         A High-School (wannabe) Polymath who likes to Code
                     </p>
                 </div>
