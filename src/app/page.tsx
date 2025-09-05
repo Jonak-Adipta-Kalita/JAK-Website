9313b71b81de88d7d77924aa48faa7a8ffe84627
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2025-09-05 11:11:13 +0530

    i mean, yeah I can always work on this; lemme get over with this pease

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ export default function Home() {
         <div>
             <div>
                 <div className="font-salsa text-center">
-                    <p className="text-[clamp(1.459rem,0.606vw+1.459rem,1.75rem)] sm:text-[clamp(3rem,2.92vw+1.6rem,4.4rem)] leading-[1] text-fg-extradark tracking-[clamp(-5.3px,-0.28vw-1px,-1px)]">
+                    <p className="text-[clamp(1.459rem,0.606vw+1.459rem,1.75rem)] sm:text-[2.3rem] md:text-[clamp(3rem,2.92vw+1.6rem,4.4rem)] leading-[1] mx-5 text-fg-extradark tracking-[clamp(-5.3px,-0.28vw-1px,-1px)]">
                         Gamer && Programmer && Musician
                     </p>
-                    <p className="hidden text-[clamp(1.25rem,1.302vw+0.625rem,1.875rem)] text-sm text-fg-light tracking-wide xl:mt-5 mt-1 text-center">
+                    <p className="text-[clamp(0.8rem,0.6vw+0.7rem,1.1rem)] sm:text-[1rem] md:text-[clamp(1.2rem,0.8vw+1rem,1.6rem)] text-fg-light tracking-tight sm:tracking-normal xl:mt-5 mt-2 text-center leading-snug">
                         A High-School (wannabe) Polymath who likes to Code
                     </p>
                 </div>
