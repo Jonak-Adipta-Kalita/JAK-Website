301ac37f591b8e1e440631ce9ac9c393bbbb40ed
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2025-09-06 15:18:54 +0530

    lemme setup an orange curtain real quick!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 const HomePage = () => {
     return (
-        <div className="relative h-screen w-screen overflow-hidden">
-            <div className="absolute top-0 left-0 z-[-1] h-full w-full bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#121212_1px)] bg-[size:20px_20px]"></div>
+        // TODO: Setup Tailwind Dark & Light Mode for the 2 types of Portals :D
 
-            <main className="h-full max-h-screen flex items-center justify-center">
-                {/* Your stuff goes here */}
+        <div className="relative h-screen w-screen overflow-hidden">
+            <div className="absolute top-0 left-0 z-[-1] h-full w-full bg-[#000000] bg-[radial-gradient(#ffffff33_1px,var(--color-bg-lobby)_1px)] bg-[size:20px_20px]"></div>
+
+            <main className="h-full max-h-screen flex flex-col">
+                <div className="space-y-5 mt-[clamp(1.5rem,2vw+1.5rem,4.5rem)] font-salsa text-center">
+                    <p className="text-[clamp(1.459rem,0.606vw+1.459rem,1.75rem)] sm:text-[2.3rem] md:text-[clamp(3rem,2.92vw+1.6rem,4.4rem)] leading-[1] mx-5 text-fg-extradark tracking-[clamp(-5.3px,-0.28vw-1px,-1px)]">
+                        Gamer && Programmer && Musician
+                    </p>
+                    <p className="text-[clamp(0.8rem,0.6vw+0.7rem,1.1rem)] sm:text-[1rem] md:text-[clamp(1.2rem,0.8vw+1rem,1.6rem)] text-fg-extralight tracking-tight sm:tracking-normal xl:mt-5 mt-2 text-center leading-snug">
+                        A High-School (wannabe) Polymath who likes to Code
+                    </p>
+                </div>
+
+                <div className="flex grow items-center justify-center flex-col">
+                    {/* The Portals - "Explore my Interests" */}
+                </div>
             </main>
         </div>
     );
