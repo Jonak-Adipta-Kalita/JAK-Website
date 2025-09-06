dcb351d54ba2738244af8625b03bc2aaff616603
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2025-09-06 10:47:01 +0530

    hm.... how do I plop those boxes tho? absolute? or is it relative??

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 const HomePage = () => {
     return (
-        <main className="grid grid-cols-10">
-            <div className="col-span-4 bg-[#121212]"></div>
-            <div className="col-span-2 bg-white"></div>
-            <div className="col-span-4 bg-black">
-                <p>hello</p>
-            </div>
-        </main>
+        <div className="relative h-screen w-screen overflow-hidden">
+            <div className="absolute top-0 left-0 z-[-1] h-full w-full bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#121212_1px)] bg-[size:20px_20px]"></div>
+
+            <main className="h-full max-h-screen flex items-center justify-center">
+                {/* Your stuff goes here */}
+            </main>
+        </div>
     );
 };
 
