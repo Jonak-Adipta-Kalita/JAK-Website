d9c18a9740cf9472a73802fc87c7d7b728df2af2
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2026-04-11 15:17:20 +0530

    i gotta do the navigation later

--- src/app/polyglot/page.tsx
+++ src/app/polyglot/page.tsx
@@ -1,5 +1,13 @@
 const PolyglotPage = () => {
-    return <main className="">{/* Sidebar-Navbar */}</main>;
+    return (
+        <main className="overflow-auto">
+            {/* Sidebar-Navbar */}
+
+            <div className="flex h-screen items-center justify-center">
+                <div className="h-[calc(100vh-clamp(64px,40.8px+6.2vw,120px))] w-[calc(100vw-clamp(64px,40.8px+6.2vw,120px))] rounded-2xl border-2 border-white/30 bg-teal-100/20 shadow-[0_8px_32px_rgba(0,0,0,0.2)] backdrop-blur-2xl"></div>
+            </div>
+        </main>
+    );
 };
 
 export default PolyglotPage;
